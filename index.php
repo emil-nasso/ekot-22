568e4fa11acf26ccfe3af9be18f060469688ee69
Emil Andersson <emil@glesys.se>
2023-12-30 20:33:54 +0100

    Replace podcast urls.

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+
+$url = "https://api.sr.se/api/rss/pod/3795";
+$proxyUrl = "https://ekot22.ninjazombie.se";
+
 $dom = new DOMDocument();
-$dom->loadXML(file_get_contents("https://api.sr.se/api/rss/pod/3795/"));
+$dom->loadXML(file_get_contents($url));
 
 $title = $dom->getElementsByTagName('title')->item(1);
 $title->textContent = '[22:00 only] ' . $title->textContent;
@@ -15,5 +19,8 @@ foreach($dom->getElementsByTagName('item') as $seg)
 
 foreach ($nodesToDelete as $item) $item->parentNode->removeChild($item);
 
+$content = $dom->saveXML();
+
+$content = str_replace($url, $proxyUrl, $content);
 header('Content-Type: text/xml; charset=utf-8');
-echo $dom->saveXML();
+echo $content;
